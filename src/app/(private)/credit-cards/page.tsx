0ec6b6c08bf6175d9dd8c0bdd838ac7855f3a234
Gabacles <g.dalrovere@gmail.com>
2026-03-11 17:19:27 -0300

    feat: add toast notification on CreditCardsPage load and enhance Toaster component with richColors

--- src/app/(private)/credit-cards/page.tsx
+++ src/app/(private)/credit-cards/page.tsx
@@ -1,12 +1,19 @@
+"use client";
 import type { Metadata } from "next";
 import { CreditCardsView } from "@/modules/credit-cards";
+import { toast } from "sonner";
+import { useEffect } from "react";
 
-export const metadata: Metadata = {
-  title: "Cartões de Crédito | Personal Finance",
-  description: "Gerencie seus cartões de crédito e acompanhe compras, parcelamentos e assinaturas",
-};
+// export const metadata: Metadata = {
+//   title: "Cartões de Crédito | Personal Finance",
+//   description: "Gerencie seus cartões de crédito e acompanhe compras, parcelamentos e assinaturas",
+// };
 
 export default function CreditCardsPage() {
+  useEffect(() => {
+    toast.error("Bem-vindo à página de cartões de crédito!");
+  }, []);
+
   return (
     <div className="mx-6 md:mx-18 px-4 py-8 sm:px-6 lg:px-8">
       <CreditCardsView />
--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -10,6 +10,7 @@ const Toaster = ({ ...props }: ToasterProps) => {
   return (
     <Sonner
       theme={theme as ToasterProps["theme"]}
+      richColors
       className="toaster group"
       icons={{
         success: (
@@ -30,10 +31,10 @@ const Toaster = ({ ...props }: ToasterProps) => {
       }}
       style={
         {
-          "--normal-bg": "var(--popover)",
-          "--normal-text": "var(--popover-foreground)",
-          "--normal-border": "var(--border)",
-          "--border-radius": "var(--radius)",
+          "--normal-bg":      "var(--popover)",
+          "--normal-text":    "var(--popover-foreground)",
+          "--normal-border":  "var(--border)",
+          "--border-radius":  "var(--radius)",
         } as React.CSSProperties
       }
       toastOptions={{
